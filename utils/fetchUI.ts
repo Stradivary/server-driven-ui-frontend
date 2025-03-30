8f4695651ff594779adf9c52b6173f21587c7b25
ahmad_muzaki <ahmad_muzaki@telkomsel.co.id>
2025-03-30 07:20:44 +0700

    [zaky] - add var islocal on fetchUI

--- utils/fetchUI.ts
+++ utils/fetchUI.ts
@@ -1,6 +1,8 @@
 export async function fetchUI(endpoint: string) {
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-  if (!apiUrl) throw new Error("NEXT_PUBLIC_API_URL is not defined");
+  const isLocal = typeof window !== "undefined" && window.location.hostname === "localhost";
+  const apiUrl = isLocal ? "http://localhost:3001" : process.env.NEXT_PUBLIC_API_URL;
+
+  if (!apiUrl) throw new Error("API URL is not defined");
 
   const res = await fetch(`${apiUrl}/ui/${endpoint}`);
   return res.json();
